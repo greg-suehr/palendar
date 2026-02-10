2b3e156ee11aaad806cf74f46d6fcc77ec807643
greg-suehr <gregory.suehr@gmail.com>
2026-02-10 11:40:11 -0500

    Explicit route definitions for Events

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,26 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Event management  
+  root "events#index"
+
+  get "/events", to: "events#index"
+
+  get "/admin/events", to: "events#admin"
+  post "/admin/events", to: "events#create"
+
+  get "/admin/events/:id", to: "events#show"
+
+  get "/admin/events/:id/edit", to: "events#edit"
+  patch "/admin/events/:id", to: "events#update"
+  put "/admin/events/:id", to: "events#update"
+
+  delete "/admin/events/:id", to: "events#destroy"
+
+  get "/admin/events/new", to: "events#new"
+  
 end
